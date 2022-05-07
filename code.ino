0b768dd34d3ff105b0df7ae9344db2654714c9c3
Habib <hr.ece17.ruet@gmail.com>
2022-05-07 10:26:38 +0600

    Pin definitions added

---
+++ code.ino
@@ -0,0 +1,21 @@
+#define STEPPER1EN    38
+#define STEPPER1STEP  A0
+#define STEPPER1DIR   A1
+
+#define STEPPER2EN    A2
+#define STEPPER2STEP  A6
+#define STEPPER2DIR   A7
+
+#define STEPPER3EN    A8
+#define STEPPER3STEP  46
+#define STEPPER3DIR   48
+
+
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
